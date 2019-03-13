5648576f4e9b6717d7d162da0dd2261eda66c603
Anatoly Shandov <anatolyshandov@gmail.com>
2019-03-13 23:59:42 +0600

    Lesson 04,task 01 23:59 13.03

--- Lesson04/Task04_01/Program.cs
+++ Lesson04/Task04_01/Program.cs
@@ -12,7 +12,7 @@ namespace Task04_01
         {
             CompoundShape combinedFigure = new CompoundShape(new GiometricFigure[4]);
             combinedFigure.giometricFigures[0] = new Rectangle(3, 4);
-            combinedFigure.giometricFigures[1] = new Trapeze(3, 4, 1.4);
+            combinedFigure.giometricFigures[1] = new Trapeze(3, 4, 1.5);
             combinedFigure.giometricFigures[2] = new Ellipse(2, 5);
             combinedFigure.giometricFigures[3] = new Parallelogram(7, 2.4);
             Console.WriteLine(combinedFigure.CompoundSapeArea());
